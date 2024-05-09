275867e96c88bed42458c179f095f52743f2b7e6
igor-mlv <malov7704@gmail.com>
2024-05-09 17:06:51 -0400

    about us laptop version is ready

--- src/components/sections/AboutUs/AboutUs.tsx
+++ src/components/sections/AboutUs/AboutUs.tsx
@@ -1,13 +1,57 @@
 import { aboutUsSection } from "@/constants";
+import parse from 'html-react-parser';
+import Image from 'next/image';
 
 export default function AboutUs() {
-    const paragraph = aboutUsSection.paragraphs.find(paragraph => paragraph.id === 1);
+    const gridCards = "flex justify-center px-[24px] bg-[#FAF6F6] bg-opacity-[5%] rounded-[23px] border-[#63626B] border-[2px] m-[-4px] border-opacity-0 hover:border-opacity-100 transition"
+
     return (
-        <div className="flex flex-col justify-between items-start pt-[100px]">
-            <h2>About <span className="text-violet">us</span></h2>
-            <p>{paragraph && paragraph.text}</p>
-            {paragraph && <p dangerouslySetInnerHTML={{ __html: paragraph.text }}></p>}
-            <p>At <span className='text-violet'>MLV Web</span> Agency, we create unique websites from scratch.</p>
+        <div className="relative flex flex-col justify-between items-start pt-[100px] ">
+            <div className=" absolute w-[426px] h-[426px] top-0 left-[-150px] opacity-20 bg-violet rounded-full blur-[60px] z-0" />
+
+            <h2 className="z-10">{aboutUsSection.title}</h2>
+
+            <div className="w-full h-[590px] mt-[60px] grid grid-cols-3 grid-rows-2 gap-x-[20px] gap-y-[28px] z-10">
+                <div className={`${gridCards} py-[40px]`}>
+                    <p>{parse(aboutUsSection.firstParagraph.text)}</p>
+                </div>
+
+                <div className={`${gridCards} flex-col`}>
+                    <p>{parse(aboutUsSection.secondParagraph.text)}</p>
+                    <p className="pt-[10px]">{parse(aboutUsSection.secondParagraph.dollarText)}</p>
+                </div>
+
+                <div className={`${gridCards} col-span-2 col-start-1 row-start-2`}>
+                    <div className="flex flex-row justify-start items-center w-full">
+                        <p className="max-w-[540px]">{parse(aboutUsSection.thirdParagraph.text)}</p>
+                        <Image
+                            width={147}
+                            height={176}
+                            src={aboutUsSection.thirdParagraph.iconUrl}
+                            alt="50%Icon"
+                            className="ml-[40px]" />
+                    </div>
+                </div>
+
+                <div className={`bg-[#FAF6F6] bg-opacity-[5%] rounded-[23px] flex-col justify-between px-[24px] py-[24px] row-span-2 col-start-3 row-start-1 bg-[url('/assets/aboutUsSection/IgorPhotoBG.png')] bg-cover`}>
+                    <div className="relative w-full h-auto" >
+                        <Image
+                            width={364}
+                            height={485}
+                            src={aboutUsSection.igorPhotoUrl}
+                            alt={aboutUsSection.igorPhotoAlt}
+                            className="object-cover"
+                            unoptimized />
+                    </div>
+
+                    <p className="pt-[36px] text-[1.5rem]">{parse(aboutUsSection.igorPhotoTitle)}</p>
+
+                </div>
+
+            </div>
         </div>
     );
 }
+
+//this is the function that is used to remove a bug, if you deleted, the parse function will not work
+// { parse('<p className="text-violet"> </p>') }
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -16,25 +16,23 @@ export const navbarLinks = [
 export const heroSection = {
     title: "We don't just build websites - we build the future of your business online.",
     btnText: "Get free quote",
-    iconUrl: "assets/heroSection/Vector7.svg",
+    iconUrl: "/assets/heroSection/Vector7.svg",
 };
 
 export const aboutUsSection = {
     title: "About us",
-    paragraphs: [
-        {
-            id: 1,
-            text: "At <span className='text-violet'>MLV Web</span> Agency, we create unique websites from scratch."
-        },
-        {
-            id: 2,
-            text: "We increase profits by introducing online marketing tools",
-            icon: "$$=$$<span className='text-violet'>$$</span>",
-        },
-        {
-            id: 3,
-            text: "Our use of advanced technologies ensures optimal website performance.",
-            icon: "assets/aboutUsSection/50%Icon.svg",
-        },
-    ]
+    firstParagraph: {
+        text: "At <span className='text-violet'>MLV Web</span> Agency, we create unique websites from scratch.",
+    },
+    secondParagraph: {
+        text: "We increase profits by introducing online marketing tools",
+        dollarText: "$$=$$<span className='text-violet'>$$</span>",
+    },
+    thirdParagraph: {
+        text: "Our use of advanced technologies ensures optimal website performance.",
+        iconUrl: "/assets/aboutUsSection/50PercentageIcon.svg",
+    },
+    igorPhotoUrl: "/assets/aboutUsSection/IgorPicture.png",
+    igorPhotoTitle: "Founder <span className='text-violet'>Igor Malov</span>",
+    igorPhotoAlt: "Founder Igor Malov",
 };
