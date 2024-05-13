21ad90f6b9f1d2c65ed94253c1e1741335fa40f5
igor-mlv <malov7704@gmail.com>
2024-05-13 14:42:08 -0400

    about section is ready and fully responsive now

--- src/components/sections/AboutUs/AboutUs.tsx
+++ src/components/sections/AboutUs/AboutUs.tsx
@@ -3,22 +3,22 @@ import parse from 'html-react-parser';
 import Image from 'next/image';
 
 export default function AboutUs() {
-    const gridCards = "flex justify-center px-[24px] bg-[#FAF6F6] bg-opacity-[5%] rounded-[23px] border-[#63626B] border-[2px] m-[-4px] border-opacity-0 hover:border-opacity-100 transition"
+    const gridCards = "flex justify-center p-[24px] bg-[#FAF6F6] bg-opacity-[5%] rounded-[23px] border-[#63626B] border-[2px] md:m-[-4px] border-opacity-0 md:hover:border-opacity-100 transition"
 
     return (
-        <section id="aboutUs" className="relative flex flex-col justify-between items-start pt-[100px] ">
+        <section id="aboutUs" className="relative flex flex-col justify-between items-start pt-[100px] md:px-0 px-2 ">
             <div className=" absolute w-[426px] h-[426px] top-0 left-[-150px] opacity-20 bg-violet rounded-full blur-[60px] z-0" />
 
             <h2 className="z-10 px-2">{aboutUsSection.title}</h2>
 
-            <div className="w-full h-[590px] mt-[60px] grid grid-cols-3 grid-rows-2 gap-x-[20px] gap-y-[28px] z-10">
-                <div className={`${gridCards} py-[40px]`}>
+            <div className="w-full md:h-[590px] md:mt-[60px] mt-[32px] md:grid md:grid-cols-3 md:grid-rows-2 md:gap-x-[20px] md:gap-y-[28px] z-10 flex flex-col md:space-y-0 space-y-[16px]">
+                <div className={`${gridCards} md:py-[40px]`}>
                     <p>{parse(aboutUsSection.firstParagraph.text)}</p>
                 </div>
 
                 <div className={`${gridCards} flex-col`}>
                     <p>{parse(aboutUsSection.secondParagraph.text)}</p>
-                    <p className="pt-[10px]">{parse(aboutUsSection.secondParagraph.dollarText)}</p>
+                    <p className="pt-[10px] md:text-[2.5rem] text-[2rem]">{parse(aboutUsSection.secondParagraph.dollarText)}</p>
                 </div>
 
                 <div className={`${gridCards} col-span-2 col-start-1 row-start-2`}>
@@ -29,22 +29,23 @@ export default function AboutUs() {
                             height={176}
                             src={aboutUsSection.thirdParagraph.iconUrl}
                             alt="50%Icon"
-                            className="ml-[40px]" />
+                            className="md:ml-[40px] md:w-[147px] w-[85px] md:float-none" />
+
                     </div>
                 </div>
 
-                <div className={`bg-[#FAF6F6] bg-opacity-[5%] rounded-[23px] flex-col justify-between px-[24px] py-[24px] row-span-2 col-start-3 row-start-1 bg-[url('/assets/aboutUsSection/IgorPhotoBG.png')] bg-cover`}>
+                <div className={`bg-[#FAF6F6] bg-opacity-[5%] md:rounded-[23px] rounded-[16px] flex-col justify-between md:p-[24px] p-[8px] row-span-2 col-start-3 row-start-1 md:bg-[url('/assets/aboutUsSection/IgorPhotoBG.png')] bg-[url('/assets/aboutUsSection/IgorPhotoBGSmall.png')] bg-cover`}>
                     <div className="relative w-full h-auto" >
                         <Image
                             width={364}
                             height={485}
                             src={aboutUsSection.igorPhotoUrl}
                             alt={aboutUsSection.igorPhotoAlt}
-                            className="object-cover"
+                            className="md:object-cover object-cover md:rounded-[12px] rounded-[16px] md:w-[364px] w-full md:h-[485px] h-[290px]"
                             unoptimized />
                     </div>
 
-                    <p className="pt-[36px] text-[1.5rem]">{parse(aboutUsSection.igorPhotoTitle)}</p>
+                    <p className="md:pt-[36px] pt-[8px] text-[1.5rem]">{parse(aboutUsSection.igorPhotoTitle)}</p>
 
                 </div>
 
